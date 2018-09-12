5e2ed4bb7550f228a8ec924e033d720bb6d6b3e2
chichuyun <442307054@qq.com>
2018-09-12 18:13:37 +0800

    Update main.py

--- 118/main.py
+++ 118/main.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if(numRows==0): return []
+        
+        mat = [[1]]
+        for i in range(1,numRows):
+            temp = [0]+mat[i-1]+[0]
+            vec = []
+            for j in range(i+1):
+                vec.append(temp[j]+temp[j+1])
+            mat.append(vec)
+        return mat
