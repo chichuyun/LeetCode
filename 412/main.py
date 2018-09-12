95d66f106c7d3d1ceb7d99757ae0572fbc12b599
chichuyun <442307054@qq.com>
2018-09-12 18:14:12 +0800

    Update main.py

--- 412/main.py
+++ 412/main.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        list_str = []
+        for x in range(1,n+1):
+            if(x%15==0):
+                list_str.append("FizzBuzz")
+            elif(x%3==0):
+                list_str.append("Fizz")
+            elif(x%5==0):
+                list_str.append("Buzz")
+            else:
+                list_str.append(str(x))
+        return list_str
