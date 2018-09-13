8a3c46af9b4c46fa65b08f53e93fd0a7172898b5
chichuyun <442307054@qq.com>
2018-09-13 23:43:18 +0800

    Update main.py

--- 204/main.py
+++ 204/main.py
@@ -4,7 +4,28 @@ class Solution:
         :type n: int
         :rtype: int
         """
-# method(1) 这里还可以优化
+# method(1) 这里还可以优化(置零那里)
+        if(n<=2): return 0
+        matrix = [False,False,True] + (n-2)*[True]
+        x = 0
+        while((x+1)*(x+1)<=n):
+            x += 1
+            if matrix[x]==0: continue
+            num = x*x
+            while(num<=n):
+                matrix[num] = False
+                num += x
+        return sum(matrix[:n])
+
+# method(2) 大神解法(其中置零用隐形循环)
+#        if n < 2: return 0
+#        matrix = [0,0] + [1]*(n-2)
+#        for x in range(2, int(n**0.5)+1):
+#            if matrix[x] == 1:
+#                matrix[x*x:n:x] = [0]*int((n-x*x-1)/x + 1)
+#        return sum(matrix)
+
+# method(3) 乘法(效率最低)
 #        if(n<=2): return 0
 #        matrix = [False,False,True] + (n-2)*[True]
 #        x = 0
@@ -17,10 +38,3 @@ class Solution:
 #                    matrix[num] = False
 #                    num *= x
 #        return sum(matrix[:n])
-# method(2) 大神解法
-        if n < 2: return 0
-        matrix = [0,0] + [1]*(n-2)
-        for i in range(2, int(n**0.5)+1):
-            if matrix[i] == 1:
-                matrix[i*i:n:i] = [0]*int((n-i*i-1)/i + 1)
-        return sum(matrix)
