80cdea270ac59516f5aa575afe9e8d5c5d811451
chichuyun <442307054@qq.com>
2018-09-19 20:23:57 +0800

    Create main.cpp

---
+++ 24/main.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode *p=head, *t, *s, *q=new ListNode(0);
+        
+        if(!p || !p->next)
+            return p;
+
+        s = p->next;
+        while(p && p->next) {
+            t = p->next;
+            q->next = t;
+            p->next = t->next;
+            t->next = p;
+            
+            q = p;
+            p = p->next;
+        }
+        return s;
+/* 递归(变量很少)
+        ListNode* temp=new ListNode(0);
+        if(!head){
+            return NULL;
+        }
+        if(!head->next){
+            return head;
+        }
+        temp=head->next;
+        head->next=swapPairs(temp->next);
+        temp->next=head;
+        return temp;
+*/
+    }
+};
