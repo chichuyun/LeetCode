689df3a78fbcdf76c232d84ce3f8ac5b16acb334
chichuyun <442307054@qq.com>
2018-09-28 08:38:34 +0800

    Update main.py

--- 033/main.py
+++ 033/main.py
@@ -5,22 +5,21 @@ class Solution:
         :type target: int
         :rtype: int
         """
-        if len(nums)==0: return -1
+        if not nums: return -1
 
-        left = 0
-        right = len(nums) - 1
-        while(left<=right):
-            mid = (right + left)//2
-            if nums[mid]==target:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] == target:       # 二分查找
                 return mid
-            if nums[left]<=nums[mid]:
-                if nums[left]<=target and target<nums[mid]:
+            if nums[left] <= nums[mid]:   # 左边有序
+                if nums[left]<= target<= nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
-            if nums[mid]<=nums[right]:
-                if nums[mid]<target and target<=nums[right]:
+            else:                         # 右边有序
+                if nums[mid]<= target <=nums[right]:
                     left = mid + 1
                 else:
-                    right = mid - 1
+                    right = mid - 1 
         return -1
