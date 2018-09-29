c38fca7f757e6db800a9a1d4d2ab8f528e555085
chichuyun <442307054@qq.com>
2018-09-29 22:32:28 +0800

    Update main.py

--- 043/main.py
+++ 043/main.py
@@ -5,6 +5,7 @@ class Solution:
         :type num2: str
         :rtype: str
         """
+# method(1) 乘法竖乘套路
         if num1=='0' or num2=='0': return '0'
         num1_s = list(map(int, num1))
         num2_s = list(map(int, num2))
@@ -36,3 +37,26 @@ class Solution:
             res = temp_res
         return res
         
+# method(2) 不急的去进位, 随后专门进位
+'''
+        num1 = num1[::-1]; num2 = num2[::-1]
+        res = [0 for i in range(len(num1) + len(num2))]
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                res[i+j] += int(num1[i]) * int(num2[j])
+                
+        p = 0; flag = 0
+        for i in range(len(res)):
+            p = (res[i] + flag) % 10
+            flag = (res[i] + flag) // 10
+            res[i] = str(p)
+            
+        res.append(str(flag))
+        index = len(res) - 1
+        
+        while index > 0 and res[index] == '0':  #最后去除最高位的0
+            del(res[index])
+            index -= 1
+        
+        return ''.join(res[::-1])
+'''
