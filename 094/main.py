30afcb1ca27c580fd2a1d88d34a66c376d7b28b0
chichuyun <442307054@qq.com>
2018-10-07 10:37:17 +0800

    Create main.py

---
+++ 094/main.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        else:
+            return self.inorderTraversal(root.left) + \
+                   [root.val] + \
+                   self.inorderTraversal(root.right)
