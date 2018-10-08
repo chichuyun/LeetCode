5f2395906085c7f59770885d3b45e7acec1015b2
chichuyun <442307054@qq.com>
2018-10-08 18:21:11 +0800

    Update main.py

--- 027/main.py
+++ 027/main.py
@@ -5,9 +5,17 @@ class Solution:
         :type val: int
         :rtype: int
         """
-# method(1) 偷懒的方法，并没有用到双指针
+# method(1) 效率高
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        return j
+# method(2) 偷懒的方法，并没有用到双指针
+'''
         counts = nums.count(val)
         for _ in range(counts):
             nums.remove(val)
         return len(nums)
-        
+'''
