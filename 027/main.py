a640233e858e1df76248449c506c4aef0aebf25b
chichuyun <442307054@qq.com>
2018-10-08 18:21:31 +0800

    Update main.py

--- 027/main.py
+++ 027/main.py
@@ -1,21 +1,30 @@
 class Solution:
-    def removeElement(self, nums, val):
+    def removeDuplicates(self, nums):
         """
         :type nums: List[int]
-        :type val: int
         :rtype: int
         """
 # method(1) 效率高
-        j = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
+        if not nums: return 0
+        j = 1
+        temp = nums[0]
+        for i in range(1,len(nums)):
+            if nums[i] != temp:
                 nums[j] = nums[i]
+                temp = nums[i]
                 j += 1
         return j
+
 # method(2) 偷懒的方法，并没有用到双指针
 '''
-        counts = nums.count(val)
-        for _ in range(counts):
-            nums.remove(val)
-        return len(nums)
+        if(len(nums)==0): return 0
+        temp = nums[0]
+        i = 1
+        for _ in range(len(nums)-1):
+            if(nums[i]==temp):
+                del nums[i]
+            else:
+                temp = nums[i]
+                i += 1
+        return i
 '''
