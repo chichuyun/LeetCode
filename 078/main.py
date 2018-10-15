55de4ba8f4f21ad9bec85a1e3761a42d0c2b9772
chichuyun <442307054@qq.com>
2018-10-15 09:06:50 +0800

    Update main.py

--- 078/main.py
+++ 078/main.py
@@ -4,11 +4,12 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-
+# method(1) 这种方法看不到顺序,res[:]和res在循环的时候不等价
+# 直接赋值是备份,加:就不是备份了
         res = [[]]
         for num in nums :
-            for temp in res[:] :
-                x = temp[:]
+            for temp in res[:]:
+                x = temp.copy()
                 x.append(num)
                 res.append(x)
         return res
