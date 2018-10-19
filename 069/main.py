60ce4e6eacf4d4d7fc10645fbeea967b8605d0cb
chichuyun <442307054@qq.com>
2018-10-19 18:19:19 +0800

    Update main.py

--- 069/main.py
+++ 069/main.py
@@ -4,7 +4,19 @@ class Solution:
         :type x: int
         :rtype: int
         """
-# method(1) 迭代求平方根,显然自己写的效率不高
+# method(1) 二分查找
+        left, right = 0, x
+        while(left < right):
+            mid = left + (right - left)//2
+            if mid*mid <= x < (mid+1)*(mid+1) :
+                return mid
+            elif mid*mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+# method(2) 迭代求平方根,显然自己写的效率不高
+'''
         if x==0: return 0
         ans = 2
         temp = 3
@@ -12,6 +24,7 @@ class Solution:
             temp = ans
             ans = (ans + x/ans)/2
         return int(ans)
-# method(2)
+'''
+# method(3)
 #        import math
 #        return int(math.sqrt(x))
