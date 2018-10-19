23eddd9345954c134d55b07ea2664b572144a4ad
chichuyun <442307054@qq.com>
2018-10-19 21:01:09 +0800

    Update main.py

--- 153/main.py
+++ 153/main.py
@@ -33,3 +33,20 @@ class Solution:
                 end = mid
         return nums[end]
 '''
+# method(3) 之后写的
+'''
+        if len(nums) == 1 or nums[0] < nums[-1]:
+            return nums[0]
+        
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = (right + left)//2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                if nums[mid] > nums[0]:
+                    left = mid + 1
+                else:
+                    right = mid
+        return nums[right]
+'''
