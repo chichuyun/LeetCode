608e8257f5e3b33991a6d58531e25869cb0702aa
chichuyun <442307054@qq.com>
2020-02-27 20:24:22 +0800

    Create main.cpp
    add 0430

---
+++ 0430/main.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        if(!head) return head;
+        Node *c, *p, *t, *n=head;
+        
+        while(n) {
+            if(n->child) {
+                c = n->child;
+                t = n->next;
+                n->next = c;
+                c->prev = n;
+                n->child = nullptr;
+                p = flatten(c);
+                while(p && p->next) p = p->next;
+                p->next = t;
+                if(t) t->prev = p;
+            } else {
+                n = n->next;
+            }
+        }
+        
+        return head;
+    }
+};
