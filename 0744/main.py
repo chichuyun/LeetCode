e39d5e8d0ede6480204dbc80c0ddb41579002184
chichuyun <442307054@qq.com>
2020-02-27 15:22:06 +0800

    Update main.py
    simplify the code

--- 0744/main.py
+++ 0744/main.py
@@ -1,6 +1,6 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        if ord(target) >= ord(letters[-1]): return letters[0]
+        if target >= letters[-1]: return letters[0]
         
         left, right = 0, len(letters)-1
         while(left <= right):
