896ffb116f10c504d691ae31860b49dddc9d6746
chichuyun <442307054@qq.com>
2020-02-29 16:40:43 +0800

    Update main.cpp
    simplify

--- 0236/main.cpp
+++ 0236/main.cpp
@@ -10,17 +10,11 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || root == p || root == q) 
-        {
-            return root;
-        }
+        if (!root || root == p || root == q) return root;
         TreeNode *left = lowestCommonAncestor(root->left, p, q);
         TreeNode *right = lowestCommonAncestor(root->right, p, q);
-        if (left && right) 
-        {
-            return root;
-        }
-        return left ? left : right;        
+        if (left && right) return root;
+        return left ? left : right;
 
 /*
         vector<TreeNode*> nums1, nums2;
