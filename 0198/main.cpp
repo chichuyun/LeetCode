586c6e0fd01d95801f5af65d38fb87d9ee1f7caf
chichuyun <442307054@qq.com>
2020-03-18 12:07:35 +0800

    Create main.cpp
    add a python version

---
+++ 0198/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.empty()) {
+            return 0;
+        } else if(nums.size()==1) {
+            return nums[0];
+        } else if(nums.size()==2) {
+            return max(nums[0],nums[1]);
+        }
+        
+        nums[1] = max(nums[0],nums[1]);
+        for(int i=2;i<nums.size();++i) {
+            nums[i] = max(nums[i-1],nums[i-2]+nums[i]);
+        }
+
+        return nums[nums.size()-1];
+    }
+};
