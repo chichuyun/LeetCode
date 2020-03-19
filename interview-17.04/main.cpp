9a277ca76d79b536ebc23159784d50524bcec49b
chichuyun <442307054@qq.com>
2020-03-19 22:05:06 +0800

    Create main.cpp
    add interview-17.04

---
+++ interview-17.04/main.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res=0;
+        for(int i=0;i<nums.size();++i) {
+            res ^= i^nums[i];
+        }
+        return res^nums.size();
+    }
+};
