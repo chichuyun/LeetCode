559a891c5494f99df730e13c419878c19bcab3e8
chichuyun <442307054@qq.com>
2020-03-22 23:03:36 +0800

    Create main.cpp
    add interview-25

---
+++ Interview/2/interview-25/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *p = new ListNode(0), *head=p;
+        while(l1 && l2) {
+            if(l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
+        }
+        if(l1) {
+            p->next = l1;
+        } else {
+            p->next = l2;
+        }
+        return head->next;
+    }
+};
