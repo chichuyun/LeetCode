e7c0d4aa7ad075c422007f3edc105e0154c7d745
chichuyun <442307054@qq.com>
2020-03-29 02:21:13 +0800

    Update main.cpp
    simplify

--- 0260/main.cpp
+++ 0260/main.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int num=0, a=0, b=0;
+        int num=0, a=0;
         for(int n : nums) num ^= n;
-        int mask = num & (~num + 1);
-        for(int n : nums) {
-            if(n&mask) {
-                a ^= n;
-            } else {
-                b ^= n;
-            }
-        }
-        return {a, b};
+        int mask = num & (~num + 1);  // mask = num & -num  // lowbit
+        for(int n : nums) if(n&mask) a ^= n;
+        return {a, num^a};
     }
 };
