eca46994bdcd4dc235dd8ae82964651763693a2e
chichuyun <442307054@qq.com>
2020-03-29 11:31:46 +0800

    Update main.cpp
    simplify

--- 0393/main.cpp
+++ 0393/main.cpp
@@ -1,30 +1,10 @@
 class Solution {
 private:
-    bool isZero(int& a) {
-        return ~a & 0b10000000;
-    }
-    bool isOne(int& a) {
-        return ~a & 0b01000000 &&
-                a & 0b10000000;
-    }
-    bool isTwo(int& a) {
-        return ~a & 0b00100000 &&
-                a & 0b10000000 &&
-                a & 0b01000000;
-    }
-    bool isThree(int& a) {
-        return ~a & 0b00010000 &&
-                a & 0b10000000 &&
-                a & 0b01000000 &&
-                a & 0b00100000;
-    }
-    bool isFour(int& a) {
-        return ~a & 0b00001000 &&
-                a & 0b10000000 &&
-                a & 0b01000000 &&
-                a & 0b00100000 &&
-                a & 0b00010000;
-    }
+    inline bool isZero(int& a)  { return ~a & 0b10000000; }
+    inline bool isOne(int& a)   { return ~a & 0b01000000 && a & 0b10000000; }
+    inline bool isTwo(int& a)   { return ~a & 0b00100000 && (a & 0b11000000) == 0b11000000; }
+    inline bool isThree(int& a) { return ~a & 0b00010000 && (a & 0b11100000) == 0b11100000; }
+    inline bool isFour(int& a)  { return ~a & 0b00001000 && (a & 0b11110000) == 0b11110000; }
 public:
     bool validUtf8(vector<int>& data) {
         int i = 0, j = 0;
