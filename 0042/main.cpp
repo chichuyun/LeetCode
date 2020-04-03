e48c3701616a7fd1f8775ccbdeeadba359d60e5c
chichuyun <442307054@qq.com>
2020-04-04 00:07:01 +0800

    Update main.cpp
    add a new method

--- 0042/main.cpp
+++ 0042/main.cpp
@@ -24,3 +24,25 @@ public:
         return count;
     }
 };
+
+/*
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        vector<int> left(height);
+        vector<int> right(height);
+        for(int i=1;i<height.size();++i) {
+            left[i] = max(left[i],left[i-1]);
+        }
+        for(int i=height.size()-2;i>=0;--i) {
+            right[i] = max(right[i],right[i+1]);
+        }
+        int count=0;
+        for(int i=0;i<height.size();++i) {
+            count += min(left[i],right[i]) - height[i];
+        }
+
+        return count;
+    }
+};
+*/
