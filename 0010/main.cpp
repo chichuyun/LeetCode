ae267a4e39324322bb5e44552b3536b56d922632
chichuyun <442307054@qq.com>
2020-04-03 23:35:13 +0800

    Update main.cpp
    speed up

--- 0010/main.cpp
+++ 0010/main.cpp
@@ -1,20 +1,25 @@
 class Solution {
 private:
+    vector<vector<int>> nums;
+
     bool backtrace(string& s, int n, string& p, int m) {
         if(m==p.size()) return n==s.size() ? true : false;
-
-        bool first = n != s.size() && (p[m] == s[n] || p[m] == '.');
+        
+        if(nums[n][m]) return false;
+        bool flag = n != s.size() && (p[m] == s[n] || p[m] == '.');
 
         if(m+1 < p.size() && p[m+1]=='*') {
-            return backtrace(s, n, p, m+2) ||             // 0
-                   (first && backtrace(s, n+1, p, m));    // 1 or more
+            flag = backtrace(s, n, p, m+2) ||             // 0
+                   (flag && backtrace(s, n+1, p, m));    // 1 or more
         } else {
-            return first && backtrace(s, n+1, p, m+1);    // next
+            flag = flag && backtrace(s, n+1, p, m+1);    // next
         }
-        return false;
+        nums[n][m] = 1;
+        return flag;
     }
 public:
     bool isMatch(string s, string p) {
+        nums.resize(s.size()+1,vector<int>(p.size()+1,0));
         if(backtrace(s, 0, p, 0)) return true;
         return false;
     }
