a594985ec4c763d6139aaade4ff039f16fafb5f0
chichuyun <442307054@qq.com>
2020-04-08 12:07:55 +0800

    Update main.cpp

--- 0010/main.cpp
+++ 0010/main.cpp
@@ -24,3 +24,24 @@ public:
         return false;
     }
 };
+
+/*
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        vector<vector<bool>> dp(s.size()+1,vector<bool>(p.size()+1,false));
+        dp[s.size()][p.size()] = true;
+        for(int i=s.size();i>=0;--i) {
+            for(int j=p.size()-1;j>=0;--j) {
+                bool first = i < s.size() && (s[i] == p[j] || p[j] == '.');  // null or equal
+                if(j+1<p.size() && p[j+1]=='*') {
+                    dp[i][j] = (first && dp[i+1][j]) || dp[i][j+2];  // 0 or more
+                } else {
+                    dp[i][j] = first && dp[i+1][j+1];  // next
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
+*/
