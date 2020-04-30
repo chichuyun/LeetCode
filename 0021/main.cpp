6ead423873a98026e6145c6e1b063b1fb9042663
chichuyun <442307054@qq.com>
2020-05-01 00:09:07 +0800

    Update main.cpp

--- 0021/main.cpp
+++ 0021/main.cpp
@@ -9,27 +9,22 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode *p=l1, *q=l2, *head=new ListNode(0), *t=head;
-        
-        if(!p)
-            return q;
-        else if(!q)
-            return p;
-
-        while(p && q) {
-            if(p->val <= q->val) {
-                t->next = p;
-                p = p->next;
+        ListNode *head = new ListNode(0), *p = head;
+        while(l1 && l2) {
+            if(l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
             } else {
-                t->next = q;
-                q = q->next;
+                p->next = l2;
+                l2 = l2->next;
             }
-            t = t->next;
+            p = p->next;
         }
-        if(!p)
-            t->next = q;
-        else
-            t->next = p;
+        if(l1) {
+            p->next = l1;
+        } else {
+            p->next = l2;
+        }
         return head->next;
     }
 };
